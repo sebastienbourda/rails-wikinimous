113b29d6048cd822077b0685b5344ee99e531443
Sebastien Bourda <32631203+sebastienbourda@users.noreply.github.com>
2022-08-16 17:02:08 +0200

    done

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  get "/articles", to: "articles#index"
+  root "articles#index"
+  resources :tasks, except: :index
 end
